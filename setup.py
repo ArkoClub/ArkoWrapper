5be552fe7bc74b0a9613f363b8c3e44f6576c71a
Karako <karakohear@gmail.com>
2022-05-16 11:48:13 +0800

    🚀: 发布v0.0.11
    Submit Branch: main

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='arko-wrapper',
     description="给你的Python迭代器加上魔法",
-    version='0.0.10',
+    version='0.0.11',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Arko',
